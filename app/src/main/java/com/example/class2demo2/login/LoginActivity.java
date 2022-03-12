1a126f5c2943c67da396cfefa11e93f09c40cc1d
AgoatZ <sk8amit@gmail.com>
2022-03-12 19:40:13 +0200

    fix labels

--- app/src/main/java/com/example/class2demo2/login/LoginActivity.java
+++ app/src/main/java/com/example/class2demo2/login/LoginActivity.java
@@ -27,13 +27,6 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.login_menu, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (!super.onOptionsItemSelected(item)) {
             switch (item.getItemId()) {
